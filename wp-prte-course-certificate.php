a0370dd58e26ef383fcfd2811e9c6b5db5fe1b36
Pedro Renan <pedrorenan@gmail.com>
2020-06-24 09:41:17 -0300

    basic plugin

---
+++ wp-prte-course-certificate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       PRTE Course Certificate
+ * Plugin URI:        https://www.prte.com.br
+ * Description:       Esse plugin controla a geração de certificados de cursos.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pedro Renan / Osvaldo Aurélio / Nivaldo Neto / Lucas Eduardo
+ * Author URI:        https://www.prte.com.br
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
